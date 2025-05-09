fab3f6f936c1b2bac59a747344812cbbcf1bc1e0
Omar <omarramadan1222@gmail.com>
2025-05-09 03:30:00 +0300

    feat: Implement login and registration functionality with UI - Created login and registration forms with validation. - Improved UI responsiveness for authentication processes.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,11 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.androidApplication) apply false
+    alias(libs.plugins.jetbrainsKotlinAndroid) apply false
+    alias(libs.plugins.kotlin.kapt) apply false
+    alias(libs.plugins.kotlin.parcelize) apply false
+    alias(libs.plugins.androidLibrary) apply false
+    id("com.google.dagger.hilt.android") version "2.50" apply false
+    id("androidx.navigation.safeargs") version "2.7.2" apply false
+
 }
